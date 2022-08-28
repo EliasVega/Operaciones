014eeafeda14d90f3312d290e8383d77015adec6
EliasVega <discom.is@gmail.com>
2022-08-28 18:03:02 -0500

    web landlord

--- routes/landlord/web.php
+++ routes/landlord/web.php
@@ -1,23 +1,5 @@
 <?php
 
-use App\Http\Controllers\AdvanceController;
-use App\Http\Controllers\BankController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\DepartmentController;
-use App\Http\Controllers\DocumentController;
-use App\Http\Controllers\IncrementController;
-use App\Http\Controllers\MunicipalityController;
-use App\Http\Controllers\OperatingController;
-use App\Http\Controllers\OperatingPartialController;
-use App\Http\Controllers\OperationController;
-use App\Http\Controllers\PartialController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\PaymentDateController;
-use App\Http\Controllers\PaymentMethodController;
-use App\Http\Controllers\PresuntivePaymentController;
-use App\Http\Controllers\RemissionController;
-use App\Http\Controllers\RoleController;
 use App\Http\Controllers\TenantController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
